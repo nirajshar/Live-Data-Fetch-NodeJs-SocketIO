d095ea5c0e830bec2e1968d3da29551fe8ba9d12
Niraj Sharma <nirajshar67@gmail.com>
2020-07-17 16:32:34 +0530

    Added Search filter & socket emit start / stop

--- public/liveData.js
+++ public/liveData.js
@@ -16,6 +16,52 @@ socket.on('getLiveData', function (data) {
 });
 
 
+$('#search').keyup(function() {    
+
+    var input, filter, table, tr, td, i;
+        input = document.getElementById("search");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("Abandoned_Call_Log");
+        tr = table.getElementsByTagName("tr");
+
+    if( filter != '') {       
+        socket.off('getLiveData');
+    } else {
+        socket.on('getLiveData', function (data) {
+            console.log('%j', data);
+            var html = '';
+            for (var i = 0; i < data.length; i++) {
+                html += "<tr>";
+                html += "<td>" + data[i].name + "</td>";
+                html += "<td>" + data[i].documents + "</td>";
+                html += "<td>" + data[i].date_created + "</td>";
+                html += "<td>" + data[i].time_elapsed + "</td>";
+                html += "</tr>";
+            }
+            document.getElementById("tbody").innerHTML = html;
+        });
+        
+    }
+
+    // console.log(filter);
+
+    for (i = 1; i < tr.length; i++) {
+        // Hide the row initially.
+        tr[i].style.display = "none";
+    
+        td = tr[i].getElementsByTagName("td");
+        for (var j = 0; j < td.length; j++) {
+          cell = tr[i].getElementsByTagName("td")[j];
+          if (cell) {
+            if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+              break;
+            } 
+          }
+        }
+    }
+});
+
 
 
 
